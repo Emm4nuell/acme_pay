9ad1a8947c7fb1895bef460f3a42b9615fc2d2ed
Emm4nuell <eduardo.eesf@gmail.com>
2024-04-23 22:28:34 -0300

    Created class Accounts

---
+++ src/main/java/br/com/acme_pay/Accounts.java
@@ -0,0 +1,93 @@
+package br.com.acme_pay;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Accounts {
+    private Long id;
+    private Integer number;
+    private Integer agency;
+    private BigDecimal balance;
+    private Customers customers;
+    private LocalDateTime created_at;
+    private LocalDateTime update_at;
+    private boolean closed;
+
+    public Accounts() {
+    }
+
+    public Accounts(Long id, Integer number, Integer agency, BigDecimal balance, Customers customers, LocalDateTime created_at, LocalDateTime update_at, boolean closed) {
+        this.id = id;
+        this.number = number;
+        this.agency = agency;
+        this.balance = balance;
+        this.customers = customers;
+        this.created_at = created_at;
+        this.update_at = update_at;
+        this.closed = closed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getAgency() {
+        return agency;
+    }
+
+    public void setAgency(Integer agency) {
+        this.agency = agency;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Customers getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Customers customers) {
+        this.customers = customers;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(LocalDateTime update_at) {
+        this.update_at = update_at;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+}
