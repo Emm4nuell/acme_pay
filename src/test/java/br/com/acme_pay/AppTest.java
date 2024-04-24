599ffacb7110bd82d6f0a5d815011b4ac958ca44
Emm4nuell <eduardo.eesf@gmail.com>
2024-04-23 21:45:10 -0300

    Primeiro commit acme_pay

---
+++ src/test/java/br/com/acme_pay/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.acme_pay;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
