0cf0d9b64002fe02c8a4325c4898213370e07939
Eduardo Emmanuel Silva de Farias <Emm4nuell@academicos.com>
2024-04-27 16:01:48 -0300

    Feito implementação do metodos no Accounts

--- src/main/java/br/com/acme_pay/Accounts.java
+++ src/main/java/br/com/acme_pay/Accounts.java
@@ -1,14 +1,14 @@
 package br.com.acme_pay;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Accounts {
@@ -18,8 +18,42 @@ public class Accounts {
     private BigDecimal balance;
     private Customers customer;
     private List<Cards> cards;
-    private Customers customers;
     private LocalDateTime created_at;
     private LocalDateTime update_at;
     private boolean closed;
+
+    public List<String> transactions = new ArrayList<>();
+
+    public void create(Accounts accounts){
+        this.setId(accounts.id);
+        this.setNumber(accounts.number);
+        this.setAgency(accounts.agency);
+        this.setBalance(accounts.balance);
+        this.setCustomer(accounts.customer);
+        this.setCards(accounts.cards);
+        this.setCreated_at(accounts.created_at);
+        this.setUpdate_at(accounts.update_at);
+        this.setClosed(accounts.closed);
+        this.transactions.add("account created successfully" + LocalDateTime.now());
+    }
+
+    public void deposit(BigDecimal ammount){
+        this.balance = this.balance.add(ammount);
+    }
+
+    public void withdraw(BigDecimal ammount){
+        this.balance.subtract(ammount);
+    }
+
+    public void transfer(Accounts accounts, BigDecimal ammount){
+        if (checkAmmount(ammount)){
+            throw new NullPointerException("error transfer");
+        }
+        deposit(ammount);
+        accounts.balance.subtract(ammount);
+    }
+
+    public boolean checkAmmount(BigDecimal ammount){
+        return this.balance.compareTo(ammount) >= 0;
+    }
 }
--- src/main/java/br/com/acme_pay/Customers.java
+++ src/main/java/br/com/acme_pay/Customers.java
@@ -4,6 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +20,71 @@ public class Customers {
     private String email;
     private String phone;
     private String document;
+    private LocalDateTime created_at;
+    private LocalDateTime update_at;
+    private List<String> transactions = new ArrayList<>();
+
+    private List<Customers> listCustomers = new ArrayList<>();
+
+    public void create(Customers customers){
+        if (!checkeCustomers(customers.document)){
+            throw new NullPointerException("error create");
+        }
+        this.setId(customers.id);
+        this.setName(customers.name);
+        this.setEmail(customers.email);
+        this.setPhone(customers.phone);
+        this.setDocument(customers.document);
+        this.setCreated_at(customers.created_at);
+        this.setUpdate_at(customers.update_at);
+        transactions.add("create customers " + LocalDateTime.now());
+        listCustomers.add(this);
+    }
+
+    public List<Customers> list(){
+        return listCustomers;
+    }
+
+    public void delete(Long id){
+        for (Customers x : listCustomers){
+            if (x.id == id){
+                listCustomers.set(id.intValue(), new Customers());
+            }
+        }
+        throw new NullPointerException("error delete");
+    }
+
+    public void update(String document, Customers customers){
+        if (!checkeCustomers(document)){
+            throw new NullPointerException("error and update");
+        }
+    }
+
+    public Customers getByCustomerCPF(String document){
+        for (Customers x : listCustomers){
+            if (x.document.contains(document)){
+                return x;
+            }
+        }
+        throw new NullPointerException("error nullpointer");
+    }
+
+    public boolean checkeCustomers(String document){
+        for (Customers c : listCustomers){
+            if (c.document.contains(document)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
+    //create-------
+    //list----------
+    //delete -----
+    //update
+    //getByCustomerCPF------
 }
